62ac89b89e6d4e7d95903de5c8bfab43935232df
Dat Nguyen <datnguyen@onhandbi.com>
2026-02-01 19:31:36 +0700

    (refactor): inject authAPI into fixtures

--- tests/API/login.api.spec.ts
+++ tests/API/login.api.spec.ts
@@ -1,19 +1,18 @@
-import { test, expect } from '@playwright/test';
-import { AuthApi } from '../../api/AuthApi';
+import { test, expect } from '../../fixtures/APIEndpoints.fixture';
 import { logAPI } from '../../utils/logger';
+import { StatusCode } from '../../api/StatusCode';
 import * as dotenv from 'dotenv';
 dotenv.config();
 
 test.describe('API Login - Mock', () => {
 
-  test('Login success', async ({ request }) => {
-    const authApi = new AuthApi(request);
+  test('Login success', async ({ authAPI }) => {
     const credentials = { username: process.env.FAKESTOREAPI_USERNAME, password: process.env.FAKESTOREAPI_PASSWORD };
-    const response = await authApi.login(credentials);
+    const response = await authAPI.login(credentials);
 
-    test.skip(response.status() === 403, 'Skipping test due to mock API limitation - CORS limitation.');
+    test.skip(response.status() === StatusCode.FORBIDDEN, 'Skipping test due to mock API limitation - CORS limitation.');
 
-    expect(response.status()).toBe(201);// mock API returns 201 for created
+    expect(response.status()).toBe(StatusCode.CREATED);// mock API returns 201 for created
 
     let body;
     const contentType = response.headers()['content-type'];
@@ -27,14 +26,13 @@ test.describe('API Login - Mock', () => {
     expect(body.token).toBeDefined();
   });
 
-  test('Login failed - missing password', async ({ request }) => {
-    const authApi = new AuthApi(request);
+  test('Login failed - missing password', async ({ authAPI }) => {
     const credentials = { username: process.env.FAKESTOREAPI_USERNAME }; // No password provided
-    const response = await authApi.login(credentials);
+    const response = await authAPI.login(credentials);
 
-    test.skip(response.status() === 403, 'Skipping test due to mock API limitation - CORS limitation.');
+    test.skip(response.status() === StatusCode.FORBIDDEN, 'Skipping test due to mock API limitation - CORS limitation.');
 
-    expect(response.status()).toBe(400);
+    expect(response.status()).toBe(StatusCode.BAD_REQUEST);
 
     let body;
     const contentType = response.headers()['content-type'];
