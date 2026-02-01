3c6212780ecb5b90238a861cf0fcb730426ed365
Dat Nguyen <datnguyen@onhandbi.com>
2026-02-01 19:26:44 +0700

    (feat): update logger

--- utils/logger.ts
+++ utils/logger.ts
@@ -1,4 +1,44 @@
 import debug from 'debug';
 
-export const logAPI = debug('api');
-export const logUI = debug('ui');
+// If DEBUG is not set by the caller, enable `api` and `ui` by default
+if (!process.env.DEBUG) {
+	process.env.DEBUG = 'api,ui';
+}
+
+type LogFn = (...args: any[]) => void;
+
+const createLogger = (namespace: string): LogFn => {
+	const dbg = debug(namespace);
+	return (...args: any[]) => {
+		const timestamp = new Date().toISOString();
+
+		if (process.env.LOG_FORMAT === 'json') {
+			const messageParts = args.map((a) => {
+				if (a instanceof Error) return a.message;
+				if (typeof a === 'object') {
+					try {
+						return JSON.stringify(a);
+					} catch {
+						return String(a);
+					}
+				}
+				return String(a);
+			});
+			const payload: any = {
+				time: timestamp,
+				namespace,
+				message: messageParts.join(' '),
+			};
+			const extra = args.filter((a) => typeof a !== 'string' && !(a instanceof Error));
+			if (extra.length) payload.data = extra;
+			console.log(JSON.stringify(payload));
+		} else {
+			const prefix = `[${timestamp}] [${namespace}]`;
+			console.log(prefix, ...args);
+		}
+
+	};
+};
+
+export const logAPI = createLogger('api');
+export const logUI = createLogger('ui');
